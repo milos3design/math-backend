81277bbee852388ed0219092789cb03abe9f22f9
Milos <milos3design@hotmail.com>
2024-11-26 13:43:49 +0100

    djoser endpoints setup

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserAccount
 
-# Register your models here.
+@admin.register(UserAccount)
+class CustomUserAdmin(UserAdmin):
+    model = UserAccount
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_active'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
--- math_app/settings.py
+++ math_app/settings.py
@@ -155,11 +155,12 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
-    'ACTIVATION_URL': 'activation/{uid}/{token}',
+    "SEND_ACTIVATION_EMAIL": True,
+    "ACTIVATION_URL": "activation/{uid}/{token}",
     'USER_CREATE_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
 }
 
-AUTH_USER_MODEL = 'api.UserAccount'
+
+AUTH_USER_MODEL = "api.UserAccount"
